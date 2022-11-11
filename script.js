fd82d64f7b51216a8d530f4640bfe534751ffdde
SLM <sabrina.martorelli@gmail.com>
2022-11-11 14:26:59 +0000

    Corrects fortmat

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-//Variables
+
+
+//Static Variables 
+var choices = ['r', 'p', 's'];
+
+//Dinamic Variables
+
 var winner = '';
-var userPoints = 0;
-var computerPoints = 0;
 var userChoice = '';
 var ComputerChoice = '';
-var choices = ['r', 'p', 's'];
+var userPoints = 0;
+var computerPoints = 0;
 var game = '';
 
-  //Game Logic
-  /*Rock: wins against scissors, loses to paper, and ties against itself.
-  Paper: wins against rock, loses to scissors, and ties against itself.
-  Scissors: wins against paper, loses to rock, and ties against itself.
-  */
-  // Assumption 
-  // Player 1 = User
-  // Player 2 = Computer
+/*
+Game Logic
+Rock: wins against scissors, loses to paper, and ties against itself.
+Paper: wins against rock, loses to scissors, and ties against itself.
+Scissors: wins against paper, loses to rock, and ties against itself.
+ 
+Assumptions 
+Player 1 = User
+Player 2 = Computer
+*/
+
 
 //User choice 
 alert('We are playing Rock, Paper, Scissors!\nYou will ear 1 point everytime you win.\nFirst we are going to ask for your choice.\nSelect Cancel button if you want to finish.');
@@ -22,17 +30,13 @@ alert('We are playing Rock, Paper, Scissors!\nYou will ear 1 point everytime you
 var userChoice = prompt("Please, enter your choice: r for Rock, p for Paper  or s for Scissor.");
 
 
-//Repits until cancel is press
-
-
+//Repits until cancel is selected
 while (userChoice) {
 
-
- //Tranform to lowercase
- userChoice = userChoice.toLowerCase();
+  //Tranform to lowercase
+  userChoice = userChoice.toLowerCase();
 
   //Computer choice
-
   var ComputerChoice = choices[Math.floor(Math.random() * choices.length)];
 
   // Concats both choices to use in switch
@@ -75,22 +79,18 @@ while (userChoice) {
   }
 
   // Returns game results
-
   alert('Your choice: ' + userChoice + '\nComputer Choice: ' + ComputerChoice + '\nThe Winner is: ' + winner);
 
-  //Repeats choice for user untill cancel is press
-
+  //Repeats choice for user untill cancel is selected
   var userChoice = prompt("Please, enter another choice: r, p or s");
- 
+
 };
 
 
 //Returns Final Score Table
-
 alert('--Final Score--\n' + 'User Points: ' + userPoints + '\n' + 'Computer Points: ' + computerPoints + '\n');
 
 //Returns Final Score text
-
 if (userPoints > computerPoints) {
 
   alert('You are the final winner! Congrats!\nRefresh and try again!');
